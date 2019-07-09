931fc9fd95d71d183a7309da7f86b564b9afe3a4
Nathanael Weinrich <nathanaelweinrich@gmx.de>
2019-07-09 12:49:28 +0200

    ,

---
+++ build_pybind_directly.sh
@@ -0,0 +1,13 @@
+
+
+echo compiling
+
+c++ -O3 -Wall -shared -std=c++14 -fPIC -fPIC -lpng -lz -lfreetype -lm\
+    Wrapper_Code.cpp -o PyProp.so  \
+    -L $(dirname `pwd`)/lib -lTUsine -lfparser\
+    `python3-config --includes`\
+    `root-config --cflags --glibs --ldflags`\
+    -I $(dirname `pwd`)/include \
+    -I pybind11/include
+    
+
